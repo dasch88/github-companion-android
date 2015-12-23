7b77b072685c534841bde67b451f853e8ff5b909
David Aschliman <draschliman@hotmail.com>
2015-12-23 11:01:00 -0500

    Fixed material design issues on main activity

--- app/src/main/java/com/aptera/githubcompanion/app/activities/MainActivity.java
+++ app/src/main/java/com/aptera/githubcompanion/app/activities/MainActivity.java
@@ -6,24 +6,27 @@ import android.content.Loader;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.content.Intent;
-import android.widget.ArrayAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aptera.githubcompanion.R;
 import com.aptera.githubcompanion.app.BaseActivity;
-import com.aptera.githubcompanion.app.adapters.DescribableArrayAdapter;
+import com.aptera.githubcompanion.app.adapters.DescribableRecyclerAdapter;
 import com.aptera.githubcompanion.app.loaders.BitmapLoader;
 import com.aptera.githubcompanion.app.loaders.ResponseLoader;
 import com.aptera.githubcompanion.lib.businesslogic.BusinessLogicException;
 import com.aptera.githubcompanion.lib.businesslogic.IRepositoryManager;
 import com.aptera.githubcompanion.lib.businesslogic.IUserManager;
-import com.aptera.githubcompanion.lib.data.IGitHubApi;
 import com.aptera.githubcompanion.lib.data.Response;
 import com.aptera.githubcompanion.lib.model.Repository;
 import com.aptera.githubcompanion.lib.model.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -32,6 +35,9 @@ public class MainActivity extends BaseActivity {
     private static final int PROFILE_IMAGE_LOADER_ID = 0;
     private static final int REPOSITORIES_LOADER_ID = 1;
 
+    private final DescribableRecyclerAdapter<Repository> mRepositoriesAdapter =
+            new DescribableRecyclerAdapter<Repository>(R.layout.listitem_describable, R.id.txtName, R.id.txtDescription);
+
     // UI references.
     private ImageView mImgProfileImage;
     private TextView mTxtUsername;
@@ -39,7 +45,7 @@ public class MainActivity extends BaseActivity {
     private TextView mTxtEmail;
     private TextView mTxtFollowerCount;
     private TextView mTxtFollowingCount;
-    private ListView mLstRepositories;
+    private RecyclerView mLstRepositories;
 
     @Inject
     public IUserManager userManager;
@@ -50,15 +56,6 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mImgProfileImage = (ImageView) findViewById(R.id.imgProfileImage);
-        mTxtUsername = (TextView) findViewById(R.id.txtUsername);
-        mTxtName = (TextView) findViewById(R.id.txtName);
-        mTxtEmail = (TextView) findViewById(R.id.txtEmail);
-        mTxtFollowerCount = (TextView) findViewById(R.id.txtFollowerCount);
-        mTxtFollowingCount = (TextView) findViewById(R.id.txtFollowingCount);
-        mLstRepositories = (ListView) findViewById(R.id.lstRepositories);
 
         //make sure that there is a currently logged in user. if not, load up the login activity
         if(userManager.getCachedCurrentUser() == null) {
@@ -66,6 +63,20 @@ public class MainActivity extends BaseActivity {
             startActivity(loginIntent);
         }
         else {
+            setContentView(R.layout.activity_main);
+
+            mImgProfileImage = (ImageView) findViewById(R.id.imgProfileImage);
+            mTxtUsername = (TextView) findViewById(R.id.txtUsername);
+            mTxtName = (TextView) findViewById(R.id.txtName);
+            mTxtEmail = (TextView) findViewById(R.id.txtEmail);
+            mTxtFollowerCount = (TextView) findViewById(R.id.txtFollowerCount);
+            mTxtFollowingCount = (TextView) findViewById(R.id.txtFollowingCount);
+            mLstRepositories = (RecyclerView) findViewById(R.id.lstRepositories);
+
+            mLstRepositories.setLayoutManager(new LinearLayoutManager(this));
+            mLstRepositories.setAdapter(mRepositoriesAdapter);
+
+
             setCurrentUserInfo();
         }
     }
@@ -79,7 +90,7 @@ public class MainActivity extends BaseActivity {
         mTxtFollowingCount.setText(currentUser.getFollowing().toString());
 
         loadCurrentUserProfileImage(this, mImgProfileImage, currentUser.getAvatarUrl());
-        loadCurrentUserRepositories(this, repositoryManager, mLstRepositories);
+        loadCurrentUserRepositories(this, repositoryManager);
     }
 
     private void loadCurrentUserProfileImage(final Activity ctx, final ImageView imgView, final String profileImageUrl) {
@@ -102,7 +113,7 @@ public class MainActivity extends BaseActivity {
         });
     }
 
-    private void loadCurrentUserRepositories(final Activity ctx, final IRepositoryManager rMgr, final ListView lstRepositories) {
+    private void loadCurrentUserRepositories(final Activity ctx, final IRepositoryManager rMgr) {
         getLoaderManager().initLoader(REPOSITORIES_LOADER_ID, null, new LoaderManager.LoaderCallbacks<Response<Repository[]>>() {
 
             @Override
@@ -120,9 +131,8 @@ public class MainActivity extends BaseActivity {
                 if (data.hasError()) {
                     Toast.makeText(getApplicationContext(), data.getException().getMessage(), Toast.LENGTH_LONG).show();
                 } else {
-                    final DescribableArrayAdapter<Repository> adapter = new DescribableArrayAdapter<Repository>(ctx, R.layout.listitem_describable,
-                            R.id.txtName, R.id.txtDescription, data.getResult());
-                    lstRepositories.setAdapter(adapter);
+                    final List<Repository> dataList = Arrays.asList(data.getResult());
+                    mRepositoriesAdapter.setDataset(dataList);
                 }
             }
 
---
+++ app/src/main/java/com/aptera/githubcompanion/app/adapters/DescribableRecyclerAdapter.java
@@ -0,0 +1,72 @@
+package com.aptera.githubcompanion.app.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.aptera.githubcompanion.lib.model.IDescribable;
+
+import java.util.List;
+
+/**
+ * Created by daschliman on 12/22/2015.
+ */
+public class DescribableRecyclerAdapter<T extends IDescribable> extends RecyclerView.Adapter<DescribableRecyclerAdapter.ViewHolder> {
+
+    private List<T> mDataset;
+    private int mLayoutResourceId;
+    private int mPrimaryTextViewResourceId;
+    private int mSecondaryTextViewResourceId;
+
+    public void setDataset(List<T> dataset) {
+        mDataset = dataset;
+        this.notifyDataSetChanged();
+    }
+
+    public DescribableRecyclerAdapter(int layoutResourceId, int primaryTextViewResourceId, int secondaryTextViewResourceId) {
+        mLayoutResourceId = layoutResourceId;
+        mPrimaryTextViewResourceId = primaryTextViewResourceId;
+        mSecondaryTextViewResourceId = secondaryTextViewResourceId;
+    }
+
+    @Override
+    public DescribableRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //create a new view and viewholder
+        View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(mLayoutResourceId, null);
+        DescribableRecyclerAdapter.ViewHolder viewHolder = new DescribableRecyclerAdapter.ViewHolder(itemLayoutView, mPrimaryTextViewResourceId, mSecondaryTextViewResourceId);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DescribableRecyclerAdapter.ViewHolder holder, int position) {
+        if(mDataset != null) {
+            T item = mDataset.get(position);
+            holder.primaryTextView.setText(item.toString());
+            holder.secondaryTextView.setText(item.getDescription());
+            holder.secondaryTextView.setVisibility(item.getDescription() == null || item.getDescription() == "" ? View.INVISIBLE : View.VISIBLE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mDataset != null)
+            return mDataset.size();
+        else
+            return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView primaryTextView;
+        public TextView secondaryTextView;
+
+        public ViewHolder(View itemLayoutView, int primaryTextViewResourceId, int secondaryTextViewResourceId) {
+            super(itemLayoutView);
+            primaryTextView = (TextView) itemLayoutView.findViewById(primaryTextViewResourceId);
+            secondaryTextView = (TextView) itemLayoutView.findViewById(secondaryTextViewResourceId);
+        }
+    }
+
+}
